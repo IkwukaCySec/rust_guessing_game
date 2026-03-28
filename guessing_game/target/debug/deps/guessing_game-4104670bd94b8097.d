708e3f0b722f33ebf39f8f8a236ca865c7714b73
Ikwuka CySec <242934426+IkwukaCySec@users.noreply.github.com>
2026-03-28 04:13:53 -0400

    Add rand 0.8 depencies and run cargo build

---
+++ guessing_game/target/debug/deps/guessing_game-4104670bd94b8097.d
@@ -0,0 +1,5 @@
+/home/kali/CySec_repos/rust_guessing_game/guessing_game/target/debug/deps/guessing_game-4104670bd94b8097.d: src/main.rs
+
+/home/kali/CySec_repos/rust_guessing_game/guessing_game/target/debug/deps/guessing_game-4104670bd94b8097: src/main.rs
+
+src/main.rs:
---
+++ guessing_game/target/debug/deps/rand_chacha-dcba01154ee73a9c.d
@@ -0,0 +1,9 @@
+/home/kali/CySec_repos/rust_guessing_game/guessing_game/target/debug/deps/rand_chacha-dcba01154ee73a9c.d: /home/kali/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/rand_chacha-0.3.1/src/lib.rs /home/kali/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/rand_chacha-0.3.1/src/chacha.rs /home/kali/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/rand_chacha-0.3.1/src/guts.rs
+
+/home/kali/CySec_repos/rust_guessing_game/guessing_game/target/debug/deps/librand_chacha-dcba01154ee73a9c.rlib: /home/kali/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/rand_chacha-0.3.1/src/lib.rs /home/kali/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/rand_chacha-0.3.1/src/chacha.rs /home/kali/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/rand_chacha-0.3.1/src/guts.rs
+
+/home/kali/CySec_repos/rust_guessing_game/guessing_game/target/debug/deps/librand_chacha-dcba01154ee73a9c.rmeta: /home/kali/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/rand_chacha-0.3.1/src/lib.rs /home/kali/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/rand_chacha-0.3.1/src/chacha.rs /home/kali/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/rand_chacha-0.3.1/src/guts.rs
+
+/home/kali/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/rand_chacha-0.3.1/src/lib.rs:
+/home/kali/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/rand_chacha-0.3.1/src/chacha.rs:
+/home/kali/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/rand_chacha-0.3.1/src/guts.rs:
